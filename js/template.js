2f2bd22563387d59417b335c6f4672c082e6f5cb
csmtalha <csmtalha@gmail.com>
2023-09-07 21:11:54 +0500

    Init

---
+++ js/template.js
@@ -0,0 +1,23 @@
+jQuery(document).ready(function($) {
+
+	$(".headroom").headroom({
+		"tolerance": 20,
+		"offset": 50,
+		"classes": {
+			"initial": "animated",
+			"pinned": "slideDown",
+			"unpinned": "slideUp"
+		}
+	});
+
+});
+
+$(document).ready(function(e){
+    $('.search-panel .dropdown-menu').find('a').click(function(e) {
+		e.preventDefault();
+		var param = $(this).attr("href").replace("#","");
+		var concept = $(this).text();
+		$('.search-panel span#search_concept').text(concept);
+		$('.input-group #search_param').val(param);
+	});
+});
